814d69c8701299218cdd3a355c169d2d9a21b744
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-07-20 21:21:41 +0100

    Commit

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      console.log(`Task is: ${task}, Task2 is: ${task2}`);
+    }
+  
+    return [task, task2];
+  }
