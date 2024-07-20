894bf879e95d1c2c1b42397fe6f36877cd386ab4
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-07-20 21:15:45 +0100

    Initial Commit

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -2,6 +2,7 @@ export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
   }
+  
   export function getLast() {
     return ' is okay';
   }
@@ -9,5 +10,6 @@ export function taskFirst() {
   export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
+  
     return combination;
   }
