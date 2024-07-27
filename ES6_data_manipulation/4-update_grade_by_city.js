4faa0b67b8c28693fd0e391b646a8dcf0c501ab3
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-07-27 22:03:14 +0100

    Initial Commit

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const gradeEntry = newGrades.find(
-        (entry) => entry.studentId === student.id
-      );
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeEntry = newGrades.find(entry => entry.studentId === student.id);
       const grade = gradeEntry ? gradeEntry.grade : 'N/A';
-
-      // Return a new object with updated grade
       return { ...student, grade };
     });
 }
