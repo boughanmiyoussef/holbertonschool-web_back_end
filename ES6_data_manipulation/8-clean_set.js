6b73c118146d60abb9b8e30c9c39da0c7bafb94e
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-07-27 22:10:37 +0100

    Initial Commit

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,7 @@ export default function cleanset(set, startString) {
   if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
+
   const result = [];
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
