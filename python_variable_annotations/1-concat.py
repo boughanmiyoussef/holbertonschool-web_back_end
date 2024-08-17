6ce2a2d3f2f204da508ff74afdc8f89557a8e25b
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-08-17 20:30:00 +0100

    Concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -8,6 +8,7 @@ Functions:
   two strings.
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Concatenate two strings.
@@ -18,6 +19,6 @@ def concat(str1: str, str2: str) -> str:
 
     Returns:
     str: The concatenated result of `str1` and `str2`.
-
     """
     return str1 + str2
+
