49e67e99026df5d54981db6e744c9b6e0482f855
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-09-21 22:11:11 +0100

    HTTP

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,13 +2,10 @@ const express = require('express');
 
 const app = express();
 const port = 1245;
+const hostname = '127.0.0.1';
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-  //   console.log(`Example app listening at http://localhost:${port}`);
-});
-
-module.exports = app;
+module.exports = app.listen(port, hostname);
